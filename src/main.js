cf7f0ae48dbf01e272c17b01df23d8322354a418
Dmitry_Babaev <ba2evd@gmail.com>
2020-11-27 06:06:02 +0300

    Добавляет переключение точки маршрута и формы

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import TripRouteView from "./view/trip-route.js";
 import TripCostView from "./view/trip-cost.js";
 import MenuView from "./view/menu.js";
 import EventAddBtn from "./view/event-add-btn.js";
+import EventEmpty from "./view/event-empty.js";
 import FilterView from "./view/filter.js";
 import SortView from "./view/sort.js";
 import EventListView from "./view/event-list.js";
@@ -18,23 +19,59 @@ const siteHeaderTripElement = document.querySelector(`.trip-main`);
 const siteHeaderControlsElement = document.querySelector(`.trip-controls`);
 const siteMainEventsElement = document.querySelector(`.trip-events`);
 
-render(siteHeaderTripElement, new TripRouteView(events).getElement(), RenderPosition.AFTERBEGIN);
+const renderEvent = (eventListElement, event) => {
+  const eventComponent = new EventView(event);
+  const eventEditComponent = new EventEditView(event);
 
-const siteHeaderTripInfoElement = siteHeaderTripElement.querySelector(`.trip-info`);
-render(siteHeaderTripInfoElement, new TripCostView(events).getElement(), RenderPosition.
-BEFOREEND);
+  const replaceEventToForm = () => {
+    eventListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
 
-render(siteHeaderTripElement, new EventAddBtn().getElement(), RenderPosition.BEFOREEND);
+  const replaceFormToEvent = () => {
+    eventListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      evt.preventDefault();
+      replaceFormToEvent();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  eventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceEventToForm();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+
+  eventEditComponent.getElement().querySelector(`form`).addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    replaceFormToEvent();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+
+  render(eventListElement, eventComponent.getElement(event), RenderPosition.BEFOREEND);
+};
 
 render(siteHeaderControlsElement, new MenuView().getElement(), RenderPosition.AFTERBEGIN);
 render(siteHeaderControlsElement, new FilterView().getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderTripElement, new EventAddBtn().getElement(), RenderPosition.BEFOREEND);
 
-render(siteMainEventsElement, new SortView().getElement(), RenderPosition.AFTERBEGIN);
-render(siteMainEventsElement, new EventListView().getElement(), RenderPosition.BEFOREEND);
+if (events.length === 0) {
+  render(siteMainEventsElement, new EventEmpty().getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(siteHeaderTripElement, new TripRouteView(events).getElement(), RenderPosition.AFTERBEGIN);
 
-const siteMainEventsListElement = siteMainEventsElement.querySelector(`.trip-events__list`);
-for (let i = 1; i < eventCount; i++) {
-  render(siteMainEventsListElement, new EventView(events[i]).getElement(), RenderPosition.BEFOREEND);
+  const siteHeaderTripInfoElement = siteHeaderTripElement.querySelector(`.trip-info`);
+  render(siteHeaderTripInfoElement, new TripCostView(events).getElement(), RenderPosition.
+  BEFOREEND);
+
+  render(siteMainEventsElement, new SortView().getElement(), RenderPosition.AFTERBEGIN);
+
+  const eventListComponent = new EventListView();
+  render(siteMainEventsElement, eventListComponent.getElement(), RenderPosition.BEFOREEND);
+
+  for (let i = 0; i < eventCount; i++) {
+    renderEvent(eventListComponent.getElement(), events[i]);
+  }
 }
-
-render(siteMainEventsListElement, new EventEditView(events[0]).getElement(), RenderPosition.AFTERBEGIN);
--- src/util.js
+++ src/util.js
@@ -19,10 +19,13 @@ export const render = (container, element, place) => {
   }
 };
 
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
 export const createElement = (template) => {
   const newElement = document.createElement(`div`);
   newElement.innerHTML = template;
-
   return newElement.lastChild;
 };
 
